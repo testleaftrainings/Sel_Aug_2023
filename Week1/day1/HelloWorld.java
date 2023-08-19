03fb3858c8fb0a0ee4b6319684353564b24a1e17
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-08-19 19:26:44 +0530

    Add files via upload

---
+++ Week1/day1/HelloWorld.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloWorld {
+	//type main->ctrl+space->enter
+	public static void main(String[] args) {
+		//write a print statement
+		//type syso->ctrl+space->enter
+		System.out.println("Hai!! This is my Firstcode");
+	}
+
+}
